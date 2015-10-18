a5abc62b9d243af6110d1562d204f26a188b14e5
Gaurav Ramanan <gaurav@rcorp.co.in>
2015-10-18 22:55:30 +0530

    Update: dont create a new global instance of _hookers if already defined

--- src/hooker.js
+++ src/hooker.js
@@ -18,77 +18,80 @@
         return to;
     }
 
-    //Browser and node.js compatible global variable.
+    //Browser and node.js compatible global variable, if not defined previously.
     var window = this;
-    window._hookers = {};
-    var hooker = {};
+    window._hookers = window._hookers || {};
 
-    /**
-     * Triggers a hook. This is exactly like pub (publish)
-     * or Socket.IO's emit or jQuery's Trigger.
-     * All the functions registered for the hookname will be called and passed some data.
-     *
-     * @param hookName The name of the hook to trigger. All registered functions for this name will be called one by one.
-     * @param data Optional parameter to pass any data necessary to all the functions registered for the hook. Each function can modify this data and send it to the next function.
-     */
-    hooker.trigger = function(hookName, data) {
-        var prevData = [data];
+    if (typeof hooker == 'undefined'){
+        var hooker = {};
 
-        //An Array of functions (hookers) that have been registered for this particular hookName
-        var hookers = window._hookers[hookName]
+        /**
+         * Triggers a hook. This is exactly like pub (publish)
+         * or Socket.IO's emit or jQuery's Trigger.
+         * All the functions registered for the hookname will be called and passed some data.
+         *
+         * @param hookName The name of the hook to trigger. All registered functions for this name will be called one by one.
+         * @param data Optional parameter to pass any data necessary to all the functions registered for the hook. Each function can modify this data and send it to the next function.
+         */
+        hooker.trigger = function(hookName, data) {
+            var prevData = [data];
 
-        //This is needed if there are no functions
-        //registered for a hook but still it is triggered
-        if (typeof hookers!=='undefined') {
-            for (var i = 0; i < hookers.length; i++) {
-                prevData[i] = extend(data, {});
-                data = hookers[i].callback.apply(hookers[i]._this, [data, extend(prevData, [])]);
+            //An Array of functions (hookers) that have been registered for this particular hookName
+            var hookers = window._hookers[hookName]
+
+            //This is needed if there are no functions
+            //registered for a hook but still it is triggered
+            if (typeof hookers!=='undefined') {
+                for (var i = 0; i < hookers.length; i++) {
+                    prevData[i] = extend(data, {});
+                    data = hookers[i].callback.apply(hookers[i]._this, [data, extend(prevData, [])]);
+                }
             }
-        }
-        return {
-            final: data,
-            previousData: prevData
+            return {
+                final: data,
+                previousData: prevData
+            };
         };
-    };
 
 
-    /**
-     * Registers a function with a hookName, when the hookName is triggered, this function will be called along with the rest in serial order or order specified.
-     *
-     * @param callback function to be called when the hook is triggered.
-     * @param _this The context (value of `this`) inside the callback.
-     */
-    hooker.register = function(hookName, callback, _this) {
+        /**
+         * Registers a function with a hookName, when the hookName is triggered, this function will be called along with the rest in serial order or order specified.
+         *
+         * @param callback function to be called when the hook is triggered.
+         * @param _this The context (value of `this`) inside the callback.
+         */
+        hooker.register = function(hookName, callback, _this) {
 
-        //Initialise the _hookers global array if its isn't already.
-        window._hookers[hookName] = window._hookers[hookName] || [];
+            //Initialise the _hookers global array if its isn't already.
+            window._hookers[hookName] = window._hookers[hookName] || [];
 
-        //Push an object inside this array that contains a reference to the callback as well as the context(this) for the function.
-        window._hookers[hookName].push({
-            callback: callback,
-            _this: _this
-        })
-        return hooker;
-    }
-
-    //Export for Node.js
-    if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
-        module.exports = hooker;
-    }
-
-    //Export for a browser environment
-    else {
-
-        //Export for AMD
-        if (typeof define === 'function' && define.amd) {
-            define([], function() {
-                return hooker;
-            });
+            //Push an object inside this array that contains a reference to the callback as well as the context(this) for the function.
+            window._hookers[hookName].push({
+                callback: callback,
+                _this: _this
+            })
+            return hooker;
         }
 
-        //Export as a global variable
+        //Export for Node.js
+        if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+            module.exports = hooker;
+        }
+
+        //Export for a browser environment
         else {
-            window.hooker = hooker;
+
+            //Export for AMD
+            if (typeof define === 'function' && define.amd) {
+                define([], function() {
+                    return hooker;
+                });
+            }
+
+            //Export as a global variable
+            else {
+                window.hooker = hooker;
+            }
         }
     }
 })()
